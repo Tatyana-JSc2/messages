2d46fed3304d9814cbc2e2a142ffc8b0cf21a73e
Tatyana-JSc2 <tanya.tanechka912@gmail.com>
2026-09-17 22:06:22 +0300

    стили логин файл

--- src/api.jsx
+++ src/api.jsx
@@ -1,10 +1,9 @@
-const API_URL = 'https://httpbin.org/post';
-
-export async function sendSms(phone, text) {
+const API_URL = 'http://127.0.0.1:8800/api/v1/notifications/sms'
+export async function sendSms(phones, message) {
   const response = await fetch(API_URL, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ phone, text }),
+    body: JSON.stringify({ phones, message }),
   });
 
   if (!response.ok) {
